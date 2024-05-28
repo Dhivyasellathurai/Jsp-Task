f596ed3cad77cbb4d01a01e5b63fce833b2504e3
Dhivyasellathurai <dhivyasellathurai@gmail.com>
2024-05-28 17:25:11 +0530

    working jsp file

--- src/main/java/com/example/jsp/controller/BookController.java
+++ src/main/java/com/example/jsp/controller/BookController.java
@@ -1,14 +1,13 @@
 package com.example.jsp.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.view.RedirectView;
 
 import com.example.jsp.entity.Book;
 import com.example.jsp.service.BookService;
@@ -20,24 +19,19 @@ public class BookController {
 	@Autowired
 	private BookService bookService;
 
-	@GetMapping("/viewBooks")
-	public String viewBooks(Model model) {
-		model.addAttribute("books", bookService.getBooks());
-		return "view-books";
+	@GetMapping
+	private String getIndex() {
+		return "index.jsp";
 	}
 
-	@GetMapping("/addBook")
-	public String addBookView(Model model) {
-		model.addAttribute("book", new Book());
-		return "add-book";
+	@PostMapping("/save")
+	private Book createBook(@RequestBody Book book) {
+		return bookService.addBook(book);
 	}
 
-	@PostMapping("/addBook")
-	public RedirectView addBook(@ModelAttribute("book") Book book, RedirectAttributes redirectAttributes) {
-		final RedirectView redirectView = new RedirectView("/book/addBook", true);
-		Book savedBook = bookService.addBook(book);
-		redirectAttributes.addFlashAttribute("savedBook", savedBook);
-		redirectAttributes.addFlashAttribute("addBookSuccess", true);
-		return redirectView;
+	@GetMapping("/getBooks")
+	private List<Book> getAllBook() {
+		return bookService.getBooks();
 	}
+
 }
--- src/main/java/com/example/jsp/service/BookService.java
+++ src/main/java/com/example/jsp/service/BookService.java
@@ -1,5 +1,7 @@
 package com.example.jsp.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +13,7 @@ public class BookService {
 	@Autowired
 	private BookRepository bookRepository;
 
-	public Object getBooks() {
+	public List<Book> getBooks() {
 		return bookRepository.findAll();
 	}
 
